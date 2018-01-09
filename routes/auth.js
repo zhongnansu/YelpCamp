74e46c6ac0b1f8015a7e7e341bb97d9a7e552c91
EC2 Default User <ec2-user@ip-172-31-8-166.us-east-2.compute.internal>
2018-01-09 07:02:59 +0000

    testing

--- routes/auth.js
+++ routes/auth.js
@@ -5,7 +5,7 @@ var router = express.Router();
 var passport = require("passport");
 var User = require("../models/user");
 
-
+//dsadasd
 router.get("/", function(req, res) {
    res.render("landing"); 
 });
